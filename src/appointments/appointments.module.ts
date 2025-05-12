d85e85f9c54d6dbea0f6c9e2471ff8680ec1c312
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-05-11 22:52:49 -0400

    imports entities in modules

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
+import { Appointment } from './entities/appointment.entity';
 
 @Module({
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
+    imports: [TypeOrmModule.forFeature([Appointment])],
 })
 export class AppointmentsModule {}
--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { Payment } from './entities/payment.entity';
 
 @Module({
   controllers: [PaymentsController],
   providers: [PaymentsService],
+  imports: [TypeOrmModule.forFeature([Payment])],
 })
 export class PaymentsModule {}
