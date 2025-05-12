d3e343837972707f554bf593c88dd3c6b7ce1ce6
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-05-11 23:39:29 -0400

    update DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,47 @@
-export class CreateUserDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsEnum,
+  IsDateString,
+  IsOptional,
+  IsEmail,
+  Length,
+} from 'class-validator';
+import { ValidRoles } from '../../interfaces';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsEnum(ValidRoles)
+  role: ValidRoles;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
+  fullName: string;
+
+  @IsDateString()
+  dateOfBirth: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(5, 20)
+  phone?: string;
+
+  @IsOptional()
+  @IsEmail()
+  @Length(5, 100)
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(5, 255)
+  address?: string;
+}
