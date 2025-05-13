bb79b60748d2fed48db0b2eca7cd2718b51fcd2f
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-05-13 05:00:53 -0400

    create api swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +15,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Medpoint RESTFul API')
+    .setDescription('Medpoint API endpoints')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
