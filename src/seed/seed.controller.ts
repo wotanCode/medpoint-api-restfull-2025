6e2c56594fcea90c31d349533c6729d8a94bad11
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-05-13 04:54:01 -0400

    update commands

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SeedService } from './seed.service';
-import { Auth } from 'src/auth/decorators';
-import { ValidRoles } from 'src/interfaces';
+// import { Auth } from 'src/auth/decorators';
+// import { ValidRoles } from 'src/interfaces';
 
 @Controller('seed')
 export class SeedController {
